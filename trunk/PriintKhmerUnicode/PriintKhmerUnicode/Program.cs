49fa49bbf5d2584dcd51695720e0a81e4780fb32
metreysk@gmail.com <metreysk@gmail.com@c308e782-6fbd-c0ea-23d0-27948d66ac29>
2013-01-28 07:29:09 +0000

    Print Khmer Unicode characters on Printer or Windows report form

---
+++ trunk/PriintKhmerUnicode/PriintKhmerUnicode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace PriintKhmerUnicode
+{
+    static class Program
+    {
+        static PrintDocument printDoc = new PrintDocument();
+        static Font font = new Font("Kh System", 16);
+        static string text = "你好";
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());*/
+            BindPrintPageEvent();
+            Preview();
+        }
+
+        private static void BindPrintPageEvent()
+        {
+            printDoc.PrintPage += new PrintPageEventHandler(printDoc_PrintPage);
+        }
+
+        private static void Preview()
+        {
+            PrintPreviewDialog dialog = new PrintPreviewDialog();
+            dialog.Document = printDoc;
+            dialog.ShowDialog();
+        }
+
+        static void printDoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(text, font, Brushes.Black, 100.0F, 100.0F);
+        }
+    }
+}
